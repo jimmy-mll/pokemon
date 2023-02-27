e2c836234777565080d30a8a1c6471391180861f
Aerafaal <jimmy.mll@hotmail.com>
2023-02-27 17:04:07 +0100

    feat(engine): englobes Raylib texture into a normalized texture

---
+++ libs/Pokemon.Engine/Graphics/Texture.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+
+namespace Pokemon.Engine.Graphics;
+
+public sealed class Texture : IDisposable
+{
+    private static readonly Color Tint;
+    
+    private readonly Raylib_CsLo.Texture _base;
+    
+    public uint Id =>
+        _base.id;
+    
+    public int Width => 
+        _base.width;
+    
+    public int Height =>
+        _base.height;
+    
+    public int MipMaps =>
+        _base.mipmaps;
+
+    public int Format =>
+        _base.format;
+    
+    public PixelFormat PixelFormat =>
+        _base.format_;
+
+    static Texture() =>
+        Tint = Raylib.WHITE;
+
+    public Texture(string path) =>
+        _base = Raylib.LoadTexture(path);
+    
+    public void Deconstruct(out uint id, out int width, out int height, out int mipmaps, out int format, out PixelFormat pixelFormat)
+    {
+        id = Id;
+        width = Width;
+        height = Height;
+        mipmaps = MipMaps;
+        format = Format;
+        pixelFormat = PixelFormat;
+    }
+
+    public void Draw(Vector2 position, Color? tint = null) =>
+        Raylib.DrawTextureV(_base, position, tint ?? Tint);
+    
+    
+    
+    public void Dispose() =>
+        Raylib.UnloadTexture(_base);
+}
