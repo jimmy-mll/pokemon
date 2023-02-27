f6de4062fe742a81ef375064e48ebfae6570a2b0
Pample72 <sasha.klinkemallie@student.uclouvain.be>
2023-02-27 17:41:14 +0100

    feat(client): Implemented sample game

---
+++ src/Pokemon.Client/PokemonGame.cs
@@ -0,0 +1,14 @@
+﻿using Pokemon.Engine;
+using Raylib_CsLo;
+
+namespace Pokemon.Client;
+
+public class PokemonGame : Game
+{
+    protected override void Draw()
+    {
+        Raylib.ClearBackground(Raylib.RED);
+
+        base.Draw();
+    }
+}
--- src/Pokemon.Client/Program.cs
+++ src/Pokemon.Client/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Pokemon.Client;
 
-Console.WriteLine("Hello, World!");
+var game = new PokemonGame();
+game.Run();
