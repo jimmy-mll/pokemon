525b6294099059b6bb2224d745640c8f86744109
Aerafaal <jimmy.mll@hotmail.com>
2023-02-27 17:04:54 +0100

    feat(engine): add a keyboard manager

---
+++ libs/Pokemon.Engine/Input/Keyboard.cs
@@ -0,0 +1,30 @@
+﻿using Raylib_CsLo;
+
+namespace Pokemon.Engine.Input;
+
+public static class Keyboard
+{
+    public static bool IsKeyDown(KeyboardKey key) =>
+        Raylib.IsKeyDown(key);
+    
+    public static bool IsKeyUp(KeyboardKey key) =>
+        Raylib.IsKeyUp(key);
+    
+    public static bool IsKeyPressed(KeyboardKey key) =>
+        Raylib.IsKeyPressed(key);
+    
+    public static bool IsKeyReleased(KeyboardKey key) =>
+        Raylib.IsKeyReleased(key);
+
+    public static bool IsKeyComboDown(params KeyboardKey[] keys) =>
+        keys.All(IsKeyDown);
+    
+    public static bool IsKeyComboUp(params KeyboardKey[] keys) =>
+        keys.All(IsKeyUp);
+    
+    public static bool IsKeyComboPressed(params KeyboardKey[] keys) =>
+        keys.All(IsKeyPressed);
+    
+    public static bool IsKeyComboReleased(params KeyboardKey[] keys) =>
+        keys.All(IsKeyReleased);
+}
