bb56e021aca7e2c9b765cc1409783455dc311db7
Aerafaal <jimmy.mll@hotmail.com>
2023-02-27 17:29:01 +0100

    feat(engine): add mouse manager

---
+++ libs/Pokemon.Engine/Input/Mouse.cs
@@ -0,0 +1,9 @@
+﻿using Raylib_CsLo;
+
+namespace Pokemon.Engine.Input;
+
+public static class Mouse
+{
+    public static MouseState GetState() =>
+        new(Raylib.GetMousePosition(), Raylib.GetMouseWheelMoveV(), Raylib.GetGestureDetected_(), Raylib.GetMouseDelta());
+}
---
+++ libs/Pokemon.Engine/Input/MouseState.cs
@@ -0,0 +1,6 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+
+namespace Pokemon.Engine.Input;
+
+public sealed record MouseState(Vector2 Position, Vector2 Wheel, Gesture Gesture, Vector2 Delta);
