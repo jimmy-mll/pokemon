2af34a6ca83a7977c104b3785ee0e2d5bcc162a7
Pample72 <sasha.klinkemallie@student.uclouvain.be>
2023-02-27 17:38:37 +0100

    feat(engine): implemented the 'Run' method

--- libs/Pokemon.Engine/Game.cs
+++ libs/Pokemon.Engine/Game.cs
@@ -1,8 +1,41 @@
-﻿namespace Pokemon.Engine;
+﻿using Raylib_CsLo;
+using System.Diagnostics;
+using System.Reflection;
 
-public abstract class Game
+namespace Pokemon.Engine;
+
+public abstract class Game : IDisposable
 {
-    protected abstract void Update(float dt);
+    private float _lastDt;
 
-    protected abstract void Draw();
+    protected virtual void Update(float dt) { }
+    protected virtual void Draw() { }
+
+    public void Run()
+    {
+        Raylib.InitWindow(1280, 720, Assembly.GetExecutingAssembly().FullName ?? "Game");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            var sw = Stopwatch.StartNew();
+
+            Update(_lastDt);
+
+            Raylib.BeginDrawing();
+
+            Draw();
+
+            Raylib.EndDrawing();
+
+            sw.Stop();
+            _lastDt = (float)sw.Elapsed.TotalSeconds;
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    public void Dispose()
+    {
+        
+    }
 }
