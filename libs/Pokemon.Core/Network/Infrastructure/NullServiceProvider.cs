00c8edea4a9512374db069f87c547974301fb378
Aerafaal <jimmy.mll@hotmail.com>
2023-02-28 10:55:58 +0100

    feat(core): add null service provider

---
+++ libs/Pokemon.Core/Network/Infrastructure/NullServiceProvider.cs
@@ -0,0 +1,9 @@
+﻿using Pokemon.Core.Threading;
+
+namespace Pokemon.Core.Network.Infrastructure;
+
+public sealed class NullServiceProvider : Singleton<NullServiceProvider>, IServiceProvider
+{
+	public object? GetService(Type serviceType) =>
+		null;
+}
