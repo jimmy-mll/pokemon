22518b9d2ba33e12adc5a556463d1d8991df1f58
Aerafaal <jimmy.mll@hotmail.com>
2023-02-28 10:12:32 +0100

    feat(core): add singleton

---
+++ libs/Pokemon.Core/Threading/Singleton.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon.Core.Threading;
+
+public abstract class Singleton<T>
+	where T : class, new()
+{
+	private static readonly Lazy<T> LazyInstance = new(() => new T(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+	public static T Instance => 
+		LazyInstance.Value;
+}
