3c31abe5ef7d4f3a422287489b806f483b6468fa
Aerafaal <jimmy.mll@hotmail.com>
2023-02-28 09:21:54 +0100

    feat(core): add message parser

---
+++ libs/Pokemon.Core/Network/Framing/IMessageParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using Pokemon.Core.Network.Metadata;
+
+namespace Pokemon.Core.Network.Framing;
+
+/// <summary>Describes a way to parse a network messages.</summary>
+public interface IMessageParser
+{
+	/// <summary>Decodes a message from the given <paramref name="sequence" />.</summary>
+	/// <param name="sequence">The sequence buffer.</param>
+	/// <param name="message">The decoded message.</param>
+	/// <returns>Whether the message was decoded successfully.</returns>
+	bool TryDecodeMessage(ReadOnlySequence<byte> sequence, [NotNullWhen(true)] out PokemonMessage? message);
+
+	/// <summary>Encodes a network message into a <see cref="ReadOnlyMemory{T}" />.</summary>
+	/// <param name="message">The message to encode.</param>
+	/// <returns>A encoded message as an <see cref="ReadOnlyMemory{T}" />.</returns>
+	ReadOnlyMemory<byte> TryEncodeMessage(PokemonMessage message);
+}
---
+++ libs/Pokemon.Core/Network/Framing/MessageParser.cs
@@ -0,0 +1,59 @@
+﻿using System.Buffers;
+using System.Diagnostics.CodeAnalysis;
+using Pokemon.Core.Network.Factory;
+using Pokemon.Core.Network.Metadata;
+using Pokemon.Core.Serialization;
+
+namespace Pokemon.Core.Network.Framing;
+
+/// <inheritdoc />
+public sealed class MessageParser : IMessageParser
+{
+	private const byte HeaderLength = sizeof(ushort) + sizeof(int);
+
+	private readonly IMessageFactory _messageFactory;
+
+	public MessageParser(IMessageFactory messageFactory) =>
+		_messageFactory = messageFactory;
+
+	/// <inheritdoc />
+	public bool TryDecodeMessage(ReadOnlySequence<byte> sequence, [NotNullWhen(true)] out PokemonMessage? message)
+	{
+		message = null;
+
+		var reader = new PokemonReader(sequence);
+
+		if (reader.BytesAvailable < HeaderLength)
+			return false;
+
+		var messageId = reader.ReadUInt16();
+
+		var messageLength = reader.ReadInt32();
+
+		if (reader.BytesAvailable < messageLength)
+			return false;
+
+		if (!_messageFactory.TryGetMessage(messageId, out message))
+			return false;
+
+		message.Deserialize(reader);
+		return true;
+	}
+
+	/// <inheritdoc />
+	public ReadOnlyMemory<byte> TryEncodeMessage(PokemonMessage message)
+	{
+		var messageWriter = new PokemonWriter();
+		message.Serialize(messageWriter);
+
+		var payload = messageWriter.BufferAsSpan;
+
+		var writer = new PokemonWriter();
+		
+		writer.WriteUInt16(message.MessageId);
+		writer.WriteInt32(payload.Length);
+		writer.WriteSpan(payload);
+
+		return writer.BufferAsMemory;
+	}
+}
