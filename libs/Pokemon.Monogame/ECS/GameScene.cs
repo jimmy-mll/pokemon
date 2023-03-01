78d406331803098314bb8a14566cd4956a442c59
Pample72 <sasha.klinkemallie@student.uclouvain.be>
2023-03-01 01:33:16 +0100

    feat(monogame): Added the 'GameScene' class used to handle all the entities.

---
+++ libs/Pokemon.Monogame/ECS/GameScene.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Arch.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Pokemon.Monogame.ECS.Components;
+using Pokemon.Monogame.ECS.Components.Renderers;
+using Pokemon.Monogame.Services.Textures;
+
+namespace Pokemon.Monogame.ECS;
+
+public abstract class GameScene
+{
+    public World World { get; private set; }
+
+    public AbstractGame Game { get; }
+    public SpriteBatch SpriteBatch { get; private set; }
+    public ContentManager Content => Game.Content;
+    public IServiceProvider Services => Game.Services;
+    public GraphicsDevice GraphicsDevice => Game.GraphicsDevice;
+
+    public GameScene(AbstractGame game)
+    {
+        Game = game;
+    }
+
+    /// <summary>
+    /// Called when the scene is loaded
+    /// </summary>
+    protected virtual void OnLoad() { }
+
+    /// <summary>
+    /// Called when the scene is unloaded
+    /// </summary>
+    protected virtual void OnUnload() { }
+
+    /// <summary>
+    /// Called when the scene is updated
+    /// </summary>
+    protected virtual void OnUpdate(GameTime gameTime) { }
+
+    /// <summary>
+    /// Called when the scene is rendered
+    /// </summary>
+    protected virtual void OnDraw(GameTime gameTime) { }
+
+    public void Load()
+    {
+        World = World.Create();
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+
+        this.OnLoad();
+    }
+
+    public void Unload()
+    {
+        this.OnUnload();
+        World.Destroy(World);
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        this.OnUpdate(gameTime);
+    }
+
+    public void Draw(GameTime gameTime)
+    {
+        this.OnDraw(gameTime);
+
+        SpriteBatch.Begin();
+
+        var queryDesc = new QueryDescription().WithAny<IRenderer, Position, Scale>();
+        World.Query(in queryDesc, (ref IRenderer renderer, ref Position position, ref Scale scale) =>
+        {
+            renderer.Render(this, SpriteBatch, position, scale);
+        });
+
+        SpriteBatch.End();
+    }
+}
