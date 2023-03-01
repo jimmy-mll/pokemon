4b2def6c84b6edd1bb37df61f9de48f117c0c8d0
Pample72 <sasha.klinkemallie@student.uclouvain.be>
2023-03-01 01:36:34 +0100

    feat(monogame): Added a 'ITextureManagerService' used to store the game textures.

---
+++ libs/Pokemon.Monogame/Services/Textures/ITextureManagerServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Pokemon.Monogame.Services.Textures;
+
+public interface ITextureManagerServices
+{
+    void AddTexture(TextureRef textureRef, Texture2D texture);
+
+    Texture2D GetTexture(TextureRef textureRef);
+
+    TextureRef GetTextureRef(Texture2D texture);
+}
---
+++ libs/Pokemon.Monogame/Services/Textures/TextureManagerServices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pokemon.Monogame.Services.Textures;
+
+public class TextureManagerServices : ITextureManagerServices
+{
+    private readonly Dictionary<TextureRef, Texture2D> _refToTexDictionary = new();
+
+    public void AddTexture(TextureRef textureRef, Texture2D texture)
+    {
+        if (_refToTexDictionary.ContainsKey(textureRef))
+            throw new InvalidOperationException("A texture reference with the same id already exists.");
+
+        _refToTexDictionary.Add(textureRef, texture);
+    }
+
+    public Texture2D GetTexture(TextureRef textureRef)
+    {
+        return _refToTexDictionary[textureRef];
+    }
+
+    public TextureRef GetTextureRef(Texture2D texture)
+    {
+        return _refToTexDictionary.FirstOrDefault(x => x.Value == texture).Key;
+    }
+}
