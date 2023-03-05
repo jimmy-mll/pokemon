9808fa73d861431f8a9b1a1059c21362022667a8
Pample72 <sasha.klinkemallie@student.uclouvain.be>
2023-03-05 22:02:11 +0100

    feat(protocol): Added a 'PokemonWriterExtensions' to write monogame components easily.

---
+++ libs/Pokemon.Protocol/Extensions/PokemonWriterExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Pokemon.Core.Serialization;
+
+namespace Pokemon.Protocol.Extensions;
+
+public static class PokemonWriterExtensions
+{
+    public static void WriteVector2(this PokemonWriter writer, Vector2 value)
+    {
+        writer.WriteFloat(value.X);
+        writer.WriteFloat(value.Y);
+    }
+
+    public static void WriteVector3(this PokemonWriter writer, Vector3 value)
+    {
+        writer.WriteFloat(value.X);
+        writer.WriteFloat(value.Y);
+        writer.WriteFloat(value.Z);
+    }
+
+    public static void WriteVector4(this PokemonWriter writer, Vector4 value)
+    {
+        writer.WriteFloat(value.X);
+        writer.WriteFloat(value.Y);
+        writer.WriteFloat(value.Z);
+        writer.WriteFloat(value.W);
+    }
+}
