91014b78fc9e990a03c37d0ff804818ce51fb134
Jimmy Millo <jimmy.mll@hotmail.com>
2023-03-08 23:31:30 +0100

    feat(source-gen): add message factory generator

---
+++ libs/Nemesis.SourceGenerators/Network/Factory/MessageFactorySyntaxReceiver.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Nemesis.SourceGenerators.Network.Factory;
+
+internal sealed class MessageFactorySyntaxReceiver : ISyntaxContextReceiver
+{
+	private const string BaseMessageClassName = "PokemonMessage";
+
+	public List<INamedTypeSymbol> CandidateClasses { get; } = new();
+
+	public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+	{
+		if (context.Node is not ClassDeclarationSyntax classDeclarationSyntax)
+			return;
+
+		if (context.SemanticModel.GetDeclaredSymbol(classDeclarationSyntax) is not INamedTypeSymbol classSymbol)
+			return;
+
+		var baseSymbol = classSymbol.BaseType;
+
+		while (baseSymbol != null && !baseSymbol.Name.Equals(BaseMessageClassName, StringComparison.Ordinal))
+			baseSymbol = baseSymbol.BaseType;
+
+		if (baseSymbol is null || !baseSymbol.Name.Equals(BaseMessageClassName, StringComparison.Ordinal))
+			return;
+
+		CandidateClasses.Add(classSymbol);
+	}
+}
